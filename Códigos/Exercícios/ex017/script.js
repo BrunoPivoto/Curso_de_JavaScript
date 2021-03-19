cb3f52fa0de81a8bfa308cbfb69def04f328e9d7
BrunoPivoto <brunorangel@gec.inatel.br>
2021-03-19 16:53:13 -0300

    novos exemplos

---
+++ Códigos/Exercícios/ex017/script.js
@@ -0,0 +1,13 @@
+function tabuada(){
+    let num = document.getElementById("txtn")
+    let tab = document.getElementById('seltab')
+    if(num.value.value.length == 0){
+        alert("Digite um número!!!")
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        while (c<= 10{
+            let item = document.creat
+        }
+    }
+}
