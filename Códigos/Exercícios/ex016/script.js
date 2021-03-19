1fc9015319724300e6b0dfd5c92aae3aec0b4aee
BrunoPivoto <brunorangel@gec.inatel.br>
2021-03-19 13:26:07 -0300

    Update script.js

--- Códigos/Exercícios/ex016/script.js
+++ Códigos/Exercícios/ex016/script.js
@@ -5,15 +5,26 @@ function contar() {
     var res = document.getElementById('res')
 
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        alert("Caixa Vazia")
-    } else{
+               res.innerHTML="Impossibru"
+    } else {
         res.innerHTML = '<p>Contando: </p>'
         var i = Number(inicio.value)
         var f = Number(fim.value)
         var p = Number(passo.value)
-        for(var c = i; c<= f; c+=p){
-            res.innerHTML += `${c} ` 
+        if(p<=0){
+            alert("Passo inválido, vou considerar como passo 1")
+            p=1
         }
-    }
+        if (i < f) {
+            for (var c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449} ` //pega o codigo no google UNICODE
+            }
 
+        } else {
+            for (var c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449} ` //pega o codigo no google UNICODE
+            }
+        }
+        res.innerHTML += `\u{1f3c1}`
+    }
 }
